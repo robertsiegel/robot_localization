181ed022bd49311c62c859beba76bc2236a4e504
apan64 <andrew.pan@students.olin.edu>
2018-10-09 14:25:29 -0400

    switched to sending vector3

--- robot_localizer/scripts/calc_position_change.py
+++ robot_localizer/scripts/calc_position_change.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python
 from sensor_msgs.msg import LaserScan
 from location import Location
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist, Vector3, PoseWithCovariance, Pose, Point, Quarternion, TwistWithCovariance
+from geometry_msgs.msg import Twist, Vector3, PoseWithCovariance, Pose, Point, Quaternion, TwistWithCovariance
 from helper_functions import TFHelper
 from constants import TIME_THROTTLE
 import rospy, time
@@ -9,8 +10,9 @@ import rospy, time
 class CalcPosNode(object):
     def __init__(self):
         rospy.init_node('calc_pos_node')
-        self.pos_change_pub = rospy.Publisher('/pos_change', Location, queue_size=10)
-        self.prev_pos = Location(0, 0, 0) # previous odometry of position and angle
+        self.pos_change_pub = rospy.Publisher('/pos_change', Vector3, queue_size=10)
+        # self.prev_pos = Location(0, 0, 0) # previous odometry of position and angle
+        self.prev_pos = Vector3(0, 0, 0) # previous odometry of position and angle
         self.prev_time = time.time()
 
     def odometry_callback(self, msg):
@@ -20,8 +22,8 @@ class CalcPosNode(object):
             return
         self.prev_time = time.time()
         # storing and working with absolute odometry, publishing relative changes
-        new_pos = Location(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.orientation.z)
-        self.pos_change_pub.publish(new_pos.x - self.prev_pos.x, new_pos.y - self.prev_pos.y, new_pos.theta - self.prev_pos.theta)
+        new_pos = Vector3(msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.orientation.z)
+        self.pos_change_pub.publish(new_pos.x - self.prev_pos.x, new_pos.y - self.prev_pos.y, new_pos.z - self.prev_pos.z)
         self.prev_pos = new_pos
         
     def run(self):
